993726b81f4c48c22c3129e79737dcda5b4c7d8f
Xamdi Cumar Cali <hamdahiiz12@gmail.com>
2026-02-07 22:06:27 -1100

    adding filename

--- exm8.js
+++ exm8.js
@@ -24,7 +24,7 @@ const storage=multer.diskStorage({
 })
 const upload=multer({storage:storage});
 app.post("/upload2",upload.single("txtfile"),(req,res)=>{
-    res.send("uploaded successfully")
+    res.send(`uploaded successfully ${req.file.filename}`)
     // let name=req.body.name;
     // let photo=req.file.originalname;
     // let sql="insert into candidate(name,photo) values(?,?)";
